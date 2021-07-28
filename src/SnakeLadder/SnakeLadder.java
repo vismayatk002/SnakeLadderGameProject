a430f7eafa4d95c7b23daddacfd9c98c8cd5b91f
vismayatk002 <vismayatk002@gmail.com>
2021-07-28 11:50:50 +0530

    Set player start position as 0

--- src/SnakeLadder/SnakeLadder.java
+++ src/SnakeLadder/SnakeLadder.java
@@ -4,6 +4,9 @@ public class SnakeLadder {
 	
 	public static void main(String[] args) {
 		
+		// initialise position as 0
+        int position = 0;
+        
 		System.out.println("Welcome to Snake Ladder Game");
 	}
 }
