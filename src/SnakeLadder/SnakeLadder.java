6bd6630c6df397d90e1592d863b4305259f06033
vismayatk002 <vismayatk002@gmail.com>
2021-07-28 12:00:14 +0530

    Updated with Rolling dice

--- src/SnakeLadder/SnakeLadder.java
+++ src/SnakeLadder/SnakeLadder.java
@@ -1,12 +1,25 @@
 package SnakeLadder;
 
+import java.util.*;
 public class SnakeLadder {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc= new Scanner(System.in);
 		// initialise position as 0
-        int position = 0;
-        
-		System.out.println("Welcome to Snake Ladder Game");
+		int position = 0;        
+		int min = 1;
+		int max = 6;
+		System.out.print("Press 1 for Roll the Dice :");
+		int number = sc.nextInt();
+		if(number == 1){		
+		    int dieNumber = (int)(Math.random() * (max - min +1) + min);
+		    System.out.println("Dies number : " + dieNumber);
+		}
+		else{
+		
+		    System.out.println("Invalid Input");
+		
+		}
 	}
 }
